2fcb5375cff48f2f48db26fd30673cd23e373f05
Panter <panternakamoto@gmail.com>
2024-08-31 17:35:36 -0300

    add validate schema

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function validateSchema(event: NostrEvent): boolean {
+  if (1112 !== event.kind!) {
+    throw new Error(`Invalid kind ${event.kind!} for event ${event.id!}`);
+  }
+  const subKindTags = event.tags.filter((t) => 't' === t[0]!);
+  if (1 !== subKindTags.length) {
+    throw new Error(`Event must have exactly one subkind`);
+  }
+  const subkind = subKindTags[0]![1];
+  switch (subkind) {
+    case 'create-nonce':
+    case 'create-identity':
+    case 'query-voucher':
+    case 'identity-transfer':
+      return true;
+    default:
+      throw new Error(`Invalid subkind for ${subkind}`);
+  }
+}
