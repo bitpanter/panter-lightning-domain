8e8c408339ec2de3e00d6349c71396901e5dda6f
Panter <panternakamoto@gmail.com>
2024-08-31 18:31:33 -0300

    Nueva Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import '~/styles/globals.css';
 
 export const metadata = {
-  title: 'LaWallet.ar',
-  description: 'Identity Provider - LaWallet',
+  title: 'pntr.xyz',
+  description: 'Pantera Capital - fortalecemos negocios',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
